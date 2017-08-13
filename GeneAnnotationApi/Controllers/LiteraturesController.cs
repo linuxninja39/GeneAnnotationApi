e378a7202017ad6e0d762488829e991b3c43d63d
Jacob Boswell <jacob@s56.net>
2017-08-13 15:17:28 -0600

    initial gene-annotation literature

--- GeneAnnotationApi/Controllers/LiteraturesController.cs
+++ GeneAnnotationApi/Controllers/LiteraturesController.cs
@@ -25,7 +25,6 @@ namespace GeneAnnotationApi.Controllers
             _mapper = mapper;
         }
 
-        // GET: api/Genes
         [HttpGet]
         public async Task<IActionResult> GetLiteratures()
         {
@@ -35,117 +34,16 @@ namespace GeneAnnotationApi.Controllers
             return Ok(literatureDtos);
         }
 
-        // GET: api/Genes/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetGene([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var gene = await _context
-                .Gene
-                .Include(g => g.GeneName)
-                .Include(g => g.GeneLocation)
-                .Include(g => g.GeneOriginType)
-                .Include(g => g.GeneVariant)
-                    .ThenInclude(variant => variant.ZygosityType)
-                .Include(g => g.GeneVariant)
-                    .ThenInclude(variant => variant.CallType)
-                .Include(g => g.GeneVariant)
-                    .ThenInclude(variant => variant.VariantType)
-                .Include(g => g.AnnotationGene)
-                    .ThenInclude(annotationGene => annotationGene.Annotation)
-                .Include(g => g.Symbol)
-                .Include(g => g.Synonym)
-                .Include(g => g.Chromosome)
-                .SingleOrDefaultAsync(m => m.Id == id);
-
-            if (gene == null)
-            {
-                return NotFound();
-            }
-
-            var geneDto = _mapper.Map<GeneDto>(gene);
-            
-            return Ok(geneDto);
-        }
-
-        // PUT: api/Genes/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutGene([FromRoute] int id, [FromBody] Gene gene)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != gene.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(gene).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GeneExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Genes
         [HttpPost]
-        public async Task<IActionResult> PostGene([FromBody] Gene gene)
+        public async Task<IActionResult> AddLiterature([FromBody] LiteratureDto literatureDto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _context.Gene.Add(gene);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetGene", new { id = gene.Id }, gene);
+            return Ok(literatureDto);
         }
 
-        // DELETE: api/Genes/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteGene([FromRoute] int id)
+        [HttpPost("GeneVariant/{geneVariantId}")]
+        public async Task<IActionResult> AddGeneVariantLiterature(int geneVariantId, [FromBody] LiteratureDto literatureDto)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var gene = await _context.Gene.SingleOrDefaultAsync(m => m.Id == id);
-            if (gene == null)
-            {
-                return NotFound();
-            }
-
-            _context.Gene.Remove(gene);
-            await _context.SaveChangesAsync();
-
-            return Ok(gene);
-        }
-
-        private bool GeneExists(int id)
-        {
-            return _context.Gene.Any(e => e.Id == id);
+            return Ok(literatureDto);
         }
     }
 }
