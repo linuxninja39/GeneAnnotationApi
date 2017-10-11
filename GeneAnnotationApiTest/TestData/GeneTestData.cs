79c79cf7ea28775eea463800fa853f1bb1de1ee9
Jacob Boswell <jacob@s56.net>
2017-10-11 15:53:14 -0600

    work on getting migrations to add/remove data

---
+++ GeneAnnotationApiTest/TestData/GeneTestData.cs
@@ -0,0 +1,14 @@
+﻿using GeneAnnotationApi.Entities;
+
+namespace GeneAnnotationApiTest.TestData
+{
+    public static class GeneTestData
+    {
+        public static Gene[] Genes = new[]
+        {
+            new Gene { KnownFunction = "It's da greatest"},
+            new Gene { KnownFunction = "Also da greatest"},
+            new Gene { KnownFunction = "Love dat green hair"}
+        };
+    }
+}
