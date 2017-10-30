dec45031839104800f551bdf18016f06d57bc8e0
Jacob Boswell <jacob@s56.net>
2017-10-30 11:12:11 -0600

    added test and some hugo files

---
+++ GeneAnnotationApiTest/Unit/BaseEfRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using GeneAnnotationApi.Entities;
+using GeneAnnotationApi.Repositories.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace GeneAnnotationApiTest.Unit
+{
+    public class BaseEfRepositoryTest
+    {
+        [Fact]
+        public void constructorTest()
+        {
+            var o = new DbContextOptions<GeneAnnotationDBContext>();
+            var contextMock = new Mock<GeneAnnotationDBContext>(o);
+            var baseRepo = new BaseEfRepository<Gene>(contextMock.Object);
+        }
+    }
+}
