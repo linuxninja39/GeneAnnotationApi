bb38cb67d7f10df8ef76ec99ce3a8de62aeb1f60
Jacob Boswell <jacob@s56.net>
2017-11-29 14:20:01 -0700

    incremental commit

---
+++ GeneAnnotationApi/Data/LoadUcscData.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using GeneAnnotationApi.Entities;
+
+namespace GeneAnnotationApi.Data
+{
+    public class LoadUcscData
+    {
+        private readonly GeneAnnotationDBContext _context;
+        private string _fileName;
+        private FileStream _fileStream;
+        private Gene _gene;
+
+        public const int ColChromosome = 1;
+        public const int ColStart = 2;
+        public const int ColEnd = 3;
+        public const int ColSymbol = 3;
+
+        /*
+         * this file will load data from ucsc hgTables from the following columns in this order
+         * #hg19.refGene.name hg19.refGene.chrom hg19.refGene.txStart hg19.refGene.txEnd hgFixed.refLink.name
+         */
+        public LoadUcscData(GeneAnnotationDBContext context, string fileName)
+        {
+            _context = context;
+            _fileName = fileName ?? "ucsc.txt";
+        }
+
+        public void LoadFile() 
+        {
+            var path = Directory.GetCurrentDirectory();
+
+            var fullPath = path + Path.DirectorySeparatorChar + _fileName;
+
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException("File not found: " + fullPath);
+            }
+
+            _fileStream = new FileStream(fullPath, FileMode.Open);
+        }
+
+        public Gene findOrCreateGene()
+        {
+            return null;
+        }
+    }
+}
---
+++ GeneAnnotationApiTest/Integration/LoadUcscDataTest.cs
@@ -0,0 +1,19 @@
+﻿using GeneAnnotationApi.Data;
+using Xunit;
+
+namespace GeneAnnotationApiTest.Integration
+{
+    public class LoadUcscDataTest : BaseDbTest
+    {
+        [Fact]
+        public void LoadFileTest()
+        {
+            var ucscLoader = new LoadUcscData(Context, "ucsc.txt.short");
+            var ex = Record.Exception(
+                () => { ucscLoader.LoadFile(); }
+            );
+
+            Assert.Null(ex);
+        }
+    }
+}
